27465fee5befc9a07b5b4b4c02f7f688e3aa4dec
virenkarthik12 <madhuribangari.21@gmail.com>
2024-09-07 21:43:33 +0530

    thrid commit

---
+++ loops.js
@@ -0,0 +1,8 @@
+
+var fruits = ["Apple", "Mango", "Grapes", "Watermelon", "Jack Fruit", "Banana"];
+
+for (i = 0; i < fruits.length; i++) {
+
+var text = fruits[i] + "<br>";
+
+console.log(text);}
--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-console.log("hello")
-
+var cr=("DoG")
+console.log(cr.toUpperCase());
+console.log("<br>");
+console.log(cr.toLowerCase());
+console.log("<br>");
+console.Log(cr.startsWith('i'));
+console.log("<br>");
